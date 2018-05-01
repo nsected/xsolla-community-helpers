70a118f49c8b33d55dbb0f8b3cc558fc096c6cff
mustaev <mustaev123456>
2018-05-01 21:14:05 +0500

    1

--- helpers/helperBrowser.js
+++ helpers/helperBrowser.js
@@ -2,6 +2,8 @@
 
 class helperBrowser extends Helper {
      async helperBrowser(method, argument) {
+         //add helperBrowser in helpers of codecept config file
+         //use await I.helperBrowser('url', '/signin');
          return await this.helpers['WebDriverIO'].browser[method](argument)
     }
 }

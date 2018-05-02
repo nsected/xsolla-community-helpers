c948c045daf2a2978e4874b822250c6ac30c6f4e
mustaev <mustaev123456>
2018-05-03 01:55:14 +0500

    1

--- page_objects/steps_file.js
+++ page_objects/steps_file.js
@@ -24,7 +24,7 @@ module.exports = function () {
         grabDateFrom: async function (selector) {
             await this.waitForElement(selector, smartWait/1000);
             let elementText = this.grabTextFrom(selector);
-            let elementDate = chrono.parse(elementText)[0].text;
+            let elementDate = chrono.parse(elementText[0])[0].text;
             return new Date(elementDate);
         },
 
